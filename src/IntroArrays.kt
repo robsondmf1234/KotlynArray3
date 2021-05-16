ba65f13901e57c6d43559180f233954b56810557
robsondmf <robsondmf1234@gmail.com>
2021-05-16 18:13:12 -0300

    Adicionando extension function somatoria ,no arquivo IntroArrays.kt

--- src/IntroArrays.kt
+++ src/IntroArrays.kt
@@ -15,15 +15,12 @@ fun main() {
     //Função map ,vai pegar os dados e transforma para outro tipo
     //pega o salario(salarios) aplica o aumento , no final monta um salarioComAumentod do tipo Array<BigDecimal>
     val salariosComAumento: Array<BigDecimal> = salarios
-        .map { salario ->
-            if (salario < "5000.00".toBigDecimal()) {
-                salario + "500.00".toBigDecimal().setScale(2, RoundingMode.UP)
-            } else {
-                (salario * aumento).setScale(2, RoundingMode.UP)
-            }
-        }
+        .map { salario -> calculaAumentoRelativo(salario, aumento) }
         .toTypedArray()
     println(salariosComAumento.contentToString())
+
+    val gastoInicial = salariosComAumento.somatoria()
+    println("Gasto Inicial: $gastoInicial")
 
 //Maneira mais longa de fazer o Array<BigDecimal>
 //    val salarios = Array<BigDecimal>(5) { BigDecimal.ZERO }
@@ -31,19 +28,33 @@ fun main() {
 //    salarios[1] = "2000.55".toBigDecimal()
 
 
-    operacoesAgregadoras()
+    //operacoesAgregadoras()
     //utilizandoRanges()
     //IterandoSobreArrays()
     //utilizandoArrays()
     //varivaeisSemArray()
 }
 
+private fun calculaAumentoRelativo(salario: BigDecimal, aumento: BigDecimal) =
+    if (salario < "5000.00".toBigDecimal()) {
+        salario + "500.00".toBigDecimal().setScale(2, RoundingMode.UP)
+    } else {
+        (salario * aumento).setScale(2, RoundingMode.UP)
+    }
+
 //Função para criar um Array<BigDecimal>, receberá um vararg como paramentro dos salários
 fun bigDecimalArrayOf(vararg valores: String): Array<BigDecimal> {
+    //[nome do array] = [ <declaração de BigDecimal> ] [ (tamanho do array)] { [indice ->
+    //[ pega a variavel no indicie[indice] ]. tranforma para BigDecimal
     val salarios = Array<BigDecimal>(valores.size) { i ->
         valores[i].toBigDecimal()
     }
     return salarios
 }
 
+fun Array<BigDecimal>.somatoria(): BigDecimal {
+    return this.reduce { acumulador, valor ->
+        acumulador + valor
+    }
+}
 
