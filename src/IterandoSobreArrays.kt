e16c0844224a84d4b14b7bd96bc0584cefa96f86
robsondmf <robsondmf1234@gmail.com>
2021-05-16 14:32:29 -0300

    Adicionando comentarios ao arquivo IterandoSobreArrays

--- src/IterandoSobreArrays.kt
+++ src/IterandoSobreArrays.kt
@@ -1,11 +1,16 @@
 public fun IterandoSobreArrays() {
+
+    //Criando um array do tipo Double
     val salarios: DoubleArray = doubleArrayOf(1500.0, 2300.0, 5000.0, 8000.0, 10000.0)
 
+    //Aumente de 10%
     val aumento = 1.1
+    //Valor do indice
     var indice = 0
 
     //Iterando no Array
     for (salario in salarios) {
+        //Iterando sobre o aray salarios incrementando o indice para mudar o indice a ser alterado
         salarios[indice] = salario * aumento
         indice++
     }
@@ -17,7 +22,7 @@ public fun IterandoSobreArrays() {
     }
     println(salarios.contentToString())
 
-    //Itrando no array pelo index
+    //Itrando no array pelo index , [indice , salario]
     salarios.forEachIndexed { i, salario ->
         salarios[i] = salario * aumento
     }
