02cf7f26f598a7f0167d5f4638d6c8bc45068cb8
robsondmf <robsondmf1234@gmail.com>
2021-05-23 13:22:45 -0300

    Adicionando a função fold IntroArrays.kt

--- src/IntroArrays.kt
+++ src/IntroArrays.kt
@@ -6,14 +6,18 @@ fun main() {
 //RoundingMode.UP arredondamento para cima
 //sintaxe setScale(quantidade casas após virgula , método de arredondamento)
 
+// Maneira mais longa de fazer o Array<BigDecimal>
+//    val salarios = Array<BigDecimal>(5) { BigDecimal.ZERO }
+//    salarios[0] = "1500.55".toBigDecimal()
+//    salarios[1] = "2000.55".toBigDecimal()
+
     //Maneira mais simples dedeclara o Array<BigDecimal>
     val salarios = bigDecimalArrayOf("1500.55", "2000.00", "5000.00", "10000.00")
     println(salarios.contentToString())
 
-
-    val aumento = "1.1".toBigDecimal()
     //Função map ,vai pegar os dados e transforma para outro tipo
     //pega o salario(salarios) aplica o aumento , no final monta um salarioComAumentod do tipo Array<BigDecimal>
+    val aumento = "1.1".toBigDecimal()
     val salariosComAumento: Array<BigDecimal> = salarios
         .map { salario -> calculaAumentoRelativo(salario, aumento) }
         .toTypedArray()
@@ -22,10 +26,18 @@ fun main() {
     val gastoInicial = salariosComAumento.somatoria()
     println("Gasto Inicial: $gastoInicial")
 
-//Maneira mais longa de fazer o Array<BigDecimal>
-//    val salarios = Array<BigDecimal>(5) { BigDecimal.ZERO }
-//    salarios[0] = "1500.55".toBigDecimal()
-//    salarios[1] = "2000.55".toBigDecimal()
+    //Fold
+    //sntaxe bigDecimal.fold(valorInicial){acumulador,salario ->
+    //acumulador valor que vai ficar armazenando a soma
+    //salario-> e o valor no indice do array de BigDecimal(salariosComAUmento)
+    //Funcinamento: fold vai pegar o valor inicial , vai calcular o salario vezes a quantidade de meses e armazenar no acumulador,
+    //quando os indices do bigDecimal terminar, vai retornar o valor total dessa soma
+    val meses = 6.toBigDecimal()
+    val gastoTotal = salariosComAumento.fold(gastoInicial) { acumulador, salario ->
+        acumulador + (salario * meses).setScale(2, RoundingMode.UP)
+    }
+    println(gastoTotal)
+
 
 
     //operacoesAgregadoras()
@@ -42,6 +54,7 @@ private fun calculaAumentoRelativo(salario: BigDecimal, aumento: BigDecimal) =
         (salario * aumento).setScale(2, RoundingMode.UP)
     }
 
+
 //Função para criar um Array<BigDecimal>, receberá um vararg como paramentro dos salários
 fun bigDecimalArrayOf(vararg valores: String): Array<BigDecimal> {
     //[nome do array] = [ <declaração de BigDecimal> ] [ (tamanho do array)] { [indice ->
@@ -52,6 +65,11 @@ fun bigDecimalArrayOf(vararg valores: String): Array<BigDecimal> {
     return salarios
 }
 
+//This é o próprio array<BigDecimal>
+//Reduce
+//Sintaxe reduce { acc, bigDecimal ->  }
+//acc = acumulador , e o valor que vai ficar armazenando os valores que vierem do array de BigDecimal
+//bigDecimal e o valor no indice do array de BigDecimal
 fun Array<BigDecimal>.somatoria(): BigDecimal {
     return this.reduce { acumulador, valor ->
         acumulador + valor
