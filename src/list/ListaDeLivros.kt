79f177c5df34c91ed7749a93227a7d690d646b45
robsondmf <robsondmf1234@gmail.com>
2021-06-06 19:34:58 -0300

    Adionado extension function para imprimir a lista de maneira mais organizada e adicionadosorted para organizar a lista

--- src/list/ListaDeLivros.kt
+++ src/list/ListaDeLivros.kt
@@ -9,7 +9,11 @@ fun main() {
         "João Guimaraes Rosa",
         1956
     )
-
+    val livro5 = Livro(
+        "Java Use a cabeça",
+        "Desonchecido",
+        1975
+    )
     val livro2 = Livro(
         "Harry Potter",
         "Rowling",
@@ -22,7 +26,7 @@ fun main() {
         1960,
         "Abril"
     )
-    val livros: MutableList<Livro> = mutableListOf(livro1, livro2, livro3)
+    val livros: MutableList<Livro> = mutableListOf(livro1, livro2, livro3, livro5)
 
     livros.add(
         Livro(
@@ -31,5 +35,21 @@ fun main() {
             1990
         )
     )
-    println(livros)
+    livros.imprimeComMarcadores()
+
+    val ordenadoAnoPublicação: List<Livro> = livros.sorted()
+    ordenadoAnoPublicação.imprimeComMarcadores()
+
+    val ordenadoPorTitulo = livros.sortedBy { it.titulo }
+    ordenadoPorTitulo.imprimeComMarcadores()
+
+    livros.sortedBy { it.autor }.imprimeComMarcadores()
+//    println(livros)
+}
+
+fun List<Livro>.imprimeComMarcadores() {
+    val textoFormatado = this.joinToString(separator = "\n") {
+        " - ${it.titulo} de ${it.autor}"
+    }
+    println(" #### Lista de Livros ####\n$textoFormatado")
 }
